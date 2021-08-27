30f877c9c6590f61dae4fbeddab8444ab1f25f2c
smartpramod <76101095+smartpramod@users.noreply.github.com>
2021-08-27 19:34:53 +0530

    Add files via upload

---
+++ PRo 97.py
@@ -0,0 +1,37 @@
+paragraph="NUMBER GUESSING GAME"
+print(paragraph)
+
+paragraph="IT is between 12 to 24"
+print(paragraph)
+
+paragraph="YOU HAVE 7 CHANCE"
+print(paragraph)
+
+number=int(input("Enter the number: "))
+
+CHANCE=7
+
+if(number>=24):
+    print("WRONG NUMBER")
+elif(number<=12):
+    print("WRONG NUMBER")
+elif(number>=18):
+    print("WRONG NUMBER")
+elif(number<=15):
+    print("WRONG NUMBER")
+elif(number>=18):
+    print("WRONG NUMBER")
+elif(number>=17):
+    print("WRONG NUMBER")
+elif(number>=20):
+    print("WRONG NUMBER")
+elif(number>=13):
+    print("WRONG NUMBER")
+elif(number>=23):
+    print("WRONG NUMBER")
+elif(number>=14):
+    print("WRONG NUMBER")
+elif(number>=22):
+    print("WRONG NUMBER")
+
+
